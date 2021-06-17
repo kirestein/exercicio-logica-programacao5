7ec506b87594e7986edcf960690435b7775829df
= <=>
2021-06-17 11:55:02 -0300

    Somando os algarismos de um número

---
+++ somaAlgarismo/somaAlgarismo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace somaAlgarismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, soma = 0, resto = 0;
+
+            Console.WriteLine("Digite um número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n > 0)
+            {
+                
+
+                resto = n % 10;
+                soma = soma + resto;
+                n = n / 10;
+            }
+
+            Console.WriteLine(soma);
+            
+
+        }
+    }
+}
